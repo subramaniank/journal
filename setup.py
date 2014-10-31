5261a1e48a2dbd8084c43e8f3e16de01208e190e
Subramanian K <subramaniank@197BLRNB13032.local>
2014-10-31 13:49:10 +0530

    setup tox and travis

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="journal",
+    version="0.01",
+    description="Keep your journals the modern way",
+    author="K Subramanian",
+    author_email="ks_aps@yahoo.com",
+    packages=find_packages(),
+)
